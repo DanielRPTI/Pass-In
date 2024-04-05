5bcda26b65fc7bb3087a8be42167f171d146596e
DanielRPTI <drjunior988@hotmail.com>
2024-04-05 09:38:34 -0300

    Finalizando a segundao aula do projeto

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -1,12 +1,22 @@
 package rocketseat.com.passin.controllers;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.dto.attendee.AttendeeIdDTO;
+import rocketseat.com.passin.dto.attendee.AttendeeRequestDTO;
+import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
 import rocketseat.com.passin.services.EventService;
 
 @RestController
@@ -14,7 +24,8 @@ import rocketseat.com.passin.services.EventService;
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
-
+    private final AttendeeService attendeeService;
+    
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
         EventResponseDTO event = this.eventService.getEventDetail(id);
@@ -29,4 +40,20 @@ public class EventController {
 
         return ResponseEntity.created(uri).body(eventIdDTO);
     }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeIdDTO  attendeeIdDTO = this.eventService.registerAttendeeOnEvent(eventId, body);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendee(@PathVariable String id){
+        AttendeesListResponseDTO attendeesListResponse = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponse);
+    }
+
 }
--- src/main/java/rocketseat/com/passin/domain/event/Event.java
+++ src/main/java/rocketseat/com/passin/domain/event/Event.java
@@ -1,12 +1,15 @@
 package rocketseat.com.passin.domain.event;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 @Entity
 @Table(name = "events")
@@ -15,10 +18,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Event {
+    
     @Id
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String   id;
+    private String id;
 
     @Column(nullable = false)
     private String title;
@@ -29,8 +33,6 @@ public class Event {
     @Column(nullable = false, unique = true)
     private String slug;
 
-    @Column(nullable = false, name = "maximum_attendees")
+    @Column(nullable = false, name="maximum_attendees")
     private Integer maximumAttendees;
-
-
 }
